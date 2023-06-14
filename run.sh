72e334aac1fc55e0782d5c30cb0f2b6fc1740d5c
Hyunwook Jo <acerghjk@gmail.com>
2023-06-15 03:14:01 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+
+pip install transformers
+pip install wandb
+# 허깅페이스 로그인 토큰
+# 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
+from huggingface_hub import login
+import wandb
+login(token='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')
+# inisw (중요)
+# 2be184e31a96c722bfebdfe35f726042eb8e526c
+# 현욱
+# 122f007f67ba33fd04a03ee9b81489dfb42264a6
+wandb login --relogin '122f007f67ba33fd04a03ee9b81489dfb42264a6'
+
+
+git clone https://github.com/ugiugi0823/tweet-robert.git
+cd /content/tweet-robert
+
+
+python main.py --batch_size 32 \
+  --learning_rate 5e-5 \
+  --optimizer 'adam' \
+  --epochs 3 \
+  --run_name 'wow_project' \
+  --project_name 'final_project' \
+  --entity_name 'ugiugi' \
