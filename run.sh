a549ad2bd0b9e01911566f65f940615c6c6f0470
Hyunwook Jo <acerghjk@gmail.com>
2023-06-15 03:17:36 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ pip install transformers
 pip install wandb
 # 허깅페이스 로그인 토큰
 # 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
-from huggingface_hub import login
-import wandb
-login(token='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')
+
+
+huggingface-cli login --token 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
 # inisw (중요)
 # 2be184e31a96c722bfebdfe35f726042eb8e526c
 # 현욱
