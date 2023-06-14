2df1ec00919d30d63566a9b0f639038f487889a4
Hyunwook Jo <acerghjk@gmail.com>
2023-06-15 02:38:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,13 @@ from train import train
 
 
 
-
-
-
-
-
-
-
 if __name__ == '__main__':
 
   p = argparse.ArgumentParser()
-  p.add_argument("--batch_size", , type=int, default=32, help="batch_size")
+  p.add_argument("--batch_size", type=int, default=32, help="batch_size")
   p.add_argument("--learning_rate", default=5e-5, help="learning_rate")
-  p.add_argument("--optimizer", , type=str, default='adam', help="['adam', 'sgd', 'rmsprop', 'adagrad'] 중 선택")
-  p.add_argument("--epochs", , type=int, default=3, help="epochs")
+  p.add_argument("--optimizer", type=str, default='adam', help="['adam', 'sgd', 'rmsprop', 'adagrad'] 중 선택")
+  p.add_argument("--epochs", type=int, default=3, help="epochs")
 
   args = p.parse_args()
   train(args)
