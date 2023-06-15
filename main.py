ab869037c2b1dd4b75a7590992a69aa103ed7d88
Hyunwook Jo <acerghjk@gmail.com>
2023-06-15 11:53:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
   p = argparse.ArgumentParser()
   p.add_argument("--batch_size", type=int, default=32, help="batch_size")
-  p.add_argument("--learning_rate", default=5e-5, help="learning_rate")
+  p.add_argument("--learning_rate", type=float, default=5e-5, help="learning_rate")
   p.add_argument("--optimizer", type=str, default='adam', help="['adam', 'sgd', 'rmsprop', 'adagrad'] 중 선택")
   p.add_argument("--epochs", type=int, default=3, help="epochs")
   p.add_argument("--run_name", type=str, default='wow_project', help='wandb, run_name')
