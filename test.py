3799292d47688ab253a8c8dafc158228aa25de66
Hyunwook Jo <acerghjk@gmail.com>
2023-06-18 01:13:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ import time
 import torch
 import wandb
 from models import ret_model
-from utils import format_time, flat_accuracy
+from utils import format_time, flat_accuracy, get_arr
 from dataloader import ret_dataloader
-
-
+import pandas as pd
+from sklearn import metrics
 
 def test(args):
   # args
@@ -20,8 +20,8 @@ def test(args):
 
 
   # wandb
-  wandb.init(project=project_name, entity=entity_name)
-  wandb.run.name = run_name
+  # wandb.init(project=project_name, entity=entity_name)
+  # wandb.run.name = run_name
 
 
 
@@ -56,63 +56,79 @@ def test(args):
 
   y_an = []
   y_pred = []
+  print('test 데이터 로더 길이', len(test_dataloader))
   for batch in test_dataloader:
-
-      # Unpack this training batch from our dataloader.
-      #
-      # As we unpack the batch, we'll also copy each tensor to the GPU using
-      # the `to` method.
-      #
-      # `batch` contains three pytorch tensors:
-      #   [0]: input ids
-      #   [1]: attention masks
-      #   [2]: labels
-      b_input_ids = batch[0].cuda()
-      b_input_mask = batch[1].to(device)
-      b_labels = batch[2].to(device)
-
-      # Tell pytorch not to bother with constructing the compute graph during
-      # the forward pass, since this is only needed for backprop (training).
-      with torch.no_grad():
-
-          # Forward pass, calculate logit predictions.
-          # token_type_ids is the same as the "segment ids", which
-          # differentiates sentence 1 and 2 in 2-sentence tasks.
-          # The documentation for this `model` function is here:
-          # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
-          # Get the "logits" output by the model. The "logits" are the output
-          # values prior to applying an activation function like the softmax.
-          outputs = model(b_input_ids,
-                                token_type_ids=None,
-                                attention_mask=b_input_mask,
-                                labels=b_labels)
-          loss, logits = outputs['loss'], outputs['logits']
-
-      # Accumulate the test loss.
-      # total_eval_loss += loss.item()
-
-      # Move logits and labels to CPU
-      logits = logits.detach().cpu().numpy()
-      label_ids = b_labels.to('cpu').numpy()
-
-      # Calculate the accuracy for this batch of test sentences, and
-      # accumulate it over all batches.
-      total_eval_accuracy += flat_accuracy(logits, label_ids)
-      y_an.append(label_ids)
-      y_pred.append(logits)
-      print('an',type(y_an))
-      print('pred',type(y_pred))
-
-
-
-      print('an',len(y_an))
-      print('pred',len(y_pred))
     
 
+
+    # Unpack this training batch from our dataloader.
+    #
+    # As we unpack the batch, we'll also copy each tensor to the GPU using
+    # the `to` method.
+    #
+    # `batch` contains three pytorch tensors:
+    #   [0]: input ids
+    #   [1]: attention masks
+    #   [2]: labels
+    b_input_ids = batch[0].cuda()
+    b_input_mask = batch[1].to(device)
+    b_labels = batch[2].to(device)
+
+    # Tell pytorch not to bother with constructing the compute graph during
+    # the forward pass, since this is only needed for backprop (training).
+    with torch.no_grad():
+
+        # Forward pass, calculate logit predictions.
+        # token_type_ids is the same as the "segment ids", which
+        # differentiates sentence 1 and 2 in 2-sentence tasks.
+        # The documentation for this `model` function is here:
+        # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+        # Get the "logits" output by the model. The "logits" are the output
+        # values prior to applying an activation function like the softmax.
+        outputs = model(b_input_ids,
+                              token_type_ids=None,
+                              attention_mask=b_input_mask,
+                              labels=b_labels)
+        loss, logits = outputs['loss'], outputs['logits']
+
+    # Accumulate the test loss.
+    # total_eval_loss += loss.item()
+
+    # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    # Calculate the accuracy for this batch of test sentences, and
+    # accumulate it over all batches.
+    
+    total_eval_accuracy += flat_accuracy(logits, label_ids)
+    pred_flat, labels_flat = get_arr(logits, label_ids)
+    y_an.append(labels_flat.item())
+    y_pred.append(pred_flat.item())
+    # print(len(y_an))
+
+
+
+
+  print('test 완료!!!') 
+  confusion_matrix = metrics.confusion_matrix(y_an, y_pred)
+  # print(confusion_matrix.shape)
+  print(confusion_matrix)
+  
+  # y_an_v = np.array(y_an)
+  # y_an_v_t = np.transpose(y_an_v)
+
+  # y_pred_v = np.array(y_pred)
+  # y_pred_v_t = np.transpose(y_pred_v)
+  # combined_array = np.vstack((y_pred_v, y_an_v_t))
+  # df = pd.DataFrame(combined_array)
+  # df.to_csv('sdfs.csv', index=False)
+  
   # Report the final accuracy for this test run.
   avg_val_accuracy = total_eval_accuracy / len(test_dataloader)
-  wandb.log({'val_accuracy':avg_val_accuracy})
-  print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
+  # wandb.log({'val_accuracy':avg_val_accuracy})
+  # print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
+
 
 
 
