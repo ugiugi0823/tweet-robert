e5d1228fad6976c3242b7143f049fc9745631cf7
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 01:11:53 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,13 +3,17 @@ pip install transformers
 pip install wandb
 
 
-# 허깅페이스 로그인 토큰
+# 💙 3가지를 수정해 주시면 정상적으로 돌아가요! 💙
+
+
+
+# 1. 허깅페이스 로그인 토큰
 # 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
 huggingface-cli login --token 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
 
 
 
-# wandb 로그인 토큰
+# 2. wandb 로그인 토큰
 # inisw (중요)
 # 2be184e31a96c722bfebdfe35f726042eb8e526c
 # 현욱
@@ -19,7 +23,7 @@ wandb login --relogin '122f007f67ba33fd04a03ee9b81489dfb42264a6'
 
 
 
-
+# 3. 요기 아래에 --entity_name 에 이름을 wandb 아이디로 해주셔야 합니다.!!
 python main.py \
     --drive \
     --data 'train_val' \
@@ -29,7 +33,7 @@ python main.py \
     --epochs 30 \
     --run_name 'inisw08-twitter-sentiment-analysis-roberta_ep30_bs256_all' \
     --project_name 'inisw08-twitter-sentiment-analysis-roberta' \
-    --entity_name 'inisw08' \
+    --entity_name 'ugiugi' \
     --model_fold_name 'inisw08-twitter-sentiment-analysis-roberta' \
     --test_data 'test_final' \
     --test_model_name 'ugiugi/inisw08-twitter-sentiment-analysis-roberta-ep30-bs256-all' \
